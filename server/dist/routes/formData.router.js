af9aa2b5db5bcebf76aae2984720e590d378a533
Harivansh Sharma <jazzz1815@gmail.com>
2025-05-28 18:28:03 +0530

    Delete server directory

--- server/dist/routes/formData.router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const formData_contr_1 = require("../controller/formData.contr");
-const formDatarouter = (0, express_1.Router)();
-formDatarouter.post("/apply", formData_contr_1.formData);
-exports.default = formDatarouter;
--- server/src/routes/formData.router.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { formData } from "../controller/formData.contr";
-const formDatarouter = Router();
-
-formDatarouter.post("/apply", formData);
-export default formDatarouter;
